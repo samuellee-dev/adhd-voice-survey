7bd9ea535fd83aad6d750b0e7cc2ffd3d82e80af
samuellee-dev <ciaociao0617@gmail.com>
2026-04-29 15:58:03 +0900

    오른쪽 상단 고유번호 표출 삭제

--- frontend(발표용)/src/routes/+layout.svelte
+++ frontend(발표용)/src/routes/+layout.svelte
@@ -1,31 +1,5 @@
 <script>
-  import { onMount } from 'svelte';
-  import { browser } from '$app/environment';
   import '../styles.css';
-
-  let userId = '';
-
-  function refreshUserId() {
-    if (!browser) return;
-    userId = localStorage.getItem('adhd_user_id') || '';
-  }
-
-  onMount(() => {
-    refreshUserId();
-    const timer = setInterval(refreshUserId, 500);
-    window.addEventListener('storage', refreshUserId);
-    return () => {
-      clearInterval(timer);
-      window.removeEventListener('storage', refreshUserId);
-    };
-  });
 </script>
-
-{#if userId}
-  <div class="global-user-id">
-    <span>고유ID</span>
-    <strong>{userId}</strong>
-  </div>
-{/if}
 
 <slot />
